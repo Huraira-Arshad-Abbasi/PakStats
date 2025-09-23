44d0b61af8cadce3408586fcf231e8155051bca3
Huraira Arshad <hurairaabbasi647@gmail.com>
2025-09-23 11:24:28 +0500

    bg issue fixed

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,38 +1,44 @@
 import React from 'react'
 import './css/template.css'
 import Details from './Details'
+import edu from '../assets/edu1.jpg'
+import tradeImg from '../assets/trade.jpg'
+import ec from '../assets/hero.jpg'
+import tec from '../assets/tec.jpg'
+import poverty from '../assets/poverty.jpg'
+import debt from '../assets/debt.webp'
 
 const Template = ({ headings, icon, section, WhyItMatter }) => {
   const [open, setOpen] = React.useState([])
 
   const backgrounds = {
-    Education: {
-      backgroundImage: 'radial-gradient(circle at center, #31294354, var(--bg) 80%), url("/src/assets/edu1.jpg") ',
+    "Education": {
+      backgroundImage: `radial-gradient(circle at center, #31294354, var(--bg) 80%), url(${edu}) `,
       // filter: 'blur(1px) opacity(0.3)',
       backgroundPosition: 'center'
     },
     "Trade in Pakistan": {
-      backgroundImage: 'radial-gradient(circle at center, #31294354, var(--bg) 80%), url("/src/assets/trade.jpg")',
+      backgroundImage: `radial-gradient(circle at center, #31294354, var(--bg) 80%), url(${tradeImg})`,
       // filter: 'blur(1px) opacity(.3)',
       backgroundPosition: 'bottom left'
     },
-    Economy: {
-      backgroundImage: 'radial-gradient(circle at center, #31294354, var(--bg) 80%), url("/src/assets/hero.jpg")',
+    "Economy": {
+      backgroundImage: `radial-gradient(circle at center, #31294354, var(--bg) 80%), url(${ec})`,
       // filter: 'blur(1px) opacity(0.2)',
       backgroundPosition: 'top'
     },
    "Science & Technology": {
-      backgroundImage: 'radial-gradient(circle at center, #31294354, var(--bg) 80%), url("/src/assets/tec.jpg")',
+      backgroundImage: `radial-gradient(circle at center, #31294354, var(--bg) 80%), url(${tec})`,
       // filter: 'blur(.1px) opacity(0.9)',
       backgroundPosition: 'top'
     },
    "External Debt": {
-      backgroundImage: 'radial-gradient(circle at center, #31294354, var(--bg) 80%), url("/src/assets/debt.webp")',
+      backgroundImage: `radial-gradient(circle at center, #31294354, var(--bg) 80%), url(${debt})`,
       // filter: 'blur(.1px) opacity(0.4)',
       backgroundPosition: 'top'
     },
    "Poverty in Pakistan": {
-      backgroundImage: 'radial-gradient(circle at center, #31294354, var(--bg) 80%), url("/src/assets/poverty.jpg")',
+      backgroundImage: `radial-gradient(circle at center, #31294354, var(--bg) 80%), url(${poverty})`,
       filter: 'grayscale(0.6)',
       backgroundPosition: 'top'
     },
